a809882ad0b92d5ef8055c8905212ea6c38bc2d8
Vivekumar01 <vivekumar2003bsr@gmail.com>
2022-11-19 17:01:12 +0530

    Admission

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 
 
 app.get("/",async(req,res)=>{
-  res.json("Daulatram Server Deployed with Accred and Events&Activities")
+  res.json("Daulatram Server Deployed with Admission section")
 })
 
 app.use(require('./router/api'));
